d53a93dd1a94a4dc031c442687f332ebdcc8d394
Samuelsousa4509 <samuel.sousa.2009@aluno.edu.pi.gov.br>
2025-09-24 17:38:54 -0300

    Relogio digital
    relogio digtal feito em html, css e JavaScript

---
+++ projetinho/assets/js/script.js
@@ -0,0 +1,19 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sg = dateToday.getSeconds();
+
+    if(hr < 10) '0' + hr;
+    if(min < 10) '0' + min;
+    if(sg < 10) '0' + sg;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sg;
+
+})
